1e55e36f3777e95ca1f6d726e2d8cbe635d98a37
Andre Correa <andre.correa@oracle.com>
2024-08-08 11:16:21 -0300

    Release 2.0.3 (#15)
    * chore: modules references updated
    
    * chore: release notes and version bump
    
    * chore: references updated
    
    * chore: missing references updated
    
    * chore: typo fix

--- security.tf
+++ security.tf
@@ -3,14 +3,14 @@
 
 module "oci_lz_scanning" {
   count  = var.scanning_configuration != null ? 1 : 0
-  source = "git::https://github.com/oracle-quickstart/terraform-oci-cis-landing-zone-security.git//vss?ref=v0.1.5"
+  source = "git::https://github.com/oci-landing-zones/terraform-oci-modules-security.git//vss?ref=v0.1.6"
   scanning_configuration  = var.scanning_configuration
   compartments_dependency = local.compartments_dependency
 }
 
 module "oci_lz_cloud_guard" {
   count  = var.cloud_guard_configuration != null ? 1 : 0
-  source = "git::https://github.com/oracle-quickstart/terraform-oci-cis-landing-zone-security.git//cloud-guard?ref=v0.1.5"
+  source = "git::https://github.com/oci-landing-zones/terraform-oci-modules-security.git//cloud-guard?ref=v0.1.6"
   tenancy_ocid              = var.tenancy_ocid
   cloud_guard_configuration = var.cloud_guard_configuration
   compartments_dependency   = local.compartments_dependency
@@ -18,7 +18,7 @@ module "oci_lz_cloud_guard" {
 
 module "oci_lz_security_zones" {
   count  = var.security_zones_configuration != null ? 1 : 0
-  source = "git::https://github.com/oracle-quickstart/terraform-oci-cis-landing-zone-security.git//security-zones?ref=v0.1.5"
+  source = "git::https://github.com/oci-landing-zones/terraform-oci-modules-security.git//security-zones?ref=v0.1.6"
   tenancy_ocid                 = var.tenancy_ocid
   security_zones_configuration = var.security_zones_configuration
   compartments_dependency      = local.compartments_dependency
@@ -26,7 +26,7 @@ module "oci_lz_security_zones" {
 
 module "oci_lz_vaults" {
   count  = var.vaults_configuration != null ? 1 : 0
-  source = "git::https://github.com/oracle-quickstart/terraform-oci-cis-landing-zone-security.git//vaults?ref=v0.1.5"
+  source = "git::https://github.com/oci-landing-zones/terraform-oci-modules-security.git//vaults?ref=v0.1.6"
   providers = {
     oci = oci
     oci.home = oci.home
