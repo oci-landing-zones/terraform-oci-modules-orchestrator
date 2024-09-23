367b853f5b737a9010be0651096735215212a541
Andre Correa <andre.correa@oracle.com>
2024-09-23 15:20:54 -0300

    feat: modules versions updated

--- networking.tf
+++ networking.tf
@@ -4,7 +4,7 @@
 # Core networking
 module "oci_lz_network" {
   count                   = var.network_configuration != null ? 1 : 0
-  source                  = "git::https://github.com/oci-landing-zones/terraform-oci-modules-networking.git?ref=release-0.6.9"
+  source                  = "git::https://github.com/oci-landing-zones/terraform-oci-modules-networking.git?ref=v0.7.0"
   network_configuration   = var.network_configuration
   compartments_dependency = local.compartments_dependency
   network_dependency      = local.ext_dep_network_map
@@ -14,7 +14,7 @@ module "oci_lz_network" {
 # Network Load Balancers
 module "oci_lz_nlb" {
   count                   = var.nlb_configuration != null ? 1 : 0
-  source                  = "git::https://github.com/oci-landing-zones/terraform-oci-modules-networking.git//modules/nlb?ref=release-0.6.9"
+  source                  = "git::https://github.com/oci-landing-zones/terraform-oci-modules-networking.git//modules/nlb?ref=v0.7.0"
   nlb_configuration       = var.nlb_configuration
   compartments_dependency = local.compartments_dependency
   network_dependency      = local.network_dependency
