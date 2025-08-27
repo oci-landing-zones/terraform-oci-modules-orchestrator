3289955a97cb8a977f54e55befc2e9e0273d5b0c
Andre Correa <andre.correa@oracle.com>
2025-08-27 15:47:35 -0300

    fix: dupe module call removed

--- workloads.tf
+++ workloads.tf
@@ -19,8 +19,8 @@ module "oci_lz_compute" {
 }
 
 module "oci_lz_oke" {
-  count                   = var.oke_clusters_configuration != null || var.oke_workers_configuration != null ? 1 : 0
-  source                  = "git::https://github.com/oci-landing-zones/terraform-oci-modules-workloads.git//cis-oke?ref=v0.2.1"
+  depends_on = [module.oci_lz_zpr] # clusters_configuration may have ZPR attributes that must exist up front.
+  source     = "git::https://github.com/oci-landing-zones/terraform-oci-modules-workloads.git//cis-oke?ref=v0.2.1"
   clusters_configuration  = var.oke_clusters_configuration
   workers_configuration   = var.oke_workers_configuration
   compartments_dependency = local.compartments_dependency
