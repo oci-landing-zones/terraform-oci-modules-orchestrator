0e99fddadb5d117c7c8c660eae2ee6942849dcfa
Andre Correa <andre.correa@oracle.com>
2025-08-27 15:58:12 -0300

    fix: oke module call conditioned

--- workloads.tf
+++ workloads.tf
@@ -20,6 +20,7 @@ module "oci_lz_compute" {
 
 module "oci_lz_oke" {
   depends_on = [module.oci_lz_zpr] # clusters_configuration may have ZPR attributes that must exist up front.
+  count      = var.oke_clusters_configuration != null || var.oke_workers_configuration != null ? 1 : 0
   source     = "git::https://github.com/oci-landing-zones/terraform-oci-modules-workloads.git//cis-oke?ref=v0.2.1"
   clusters_configuration  = var.oke_clusters_configuration
   workers_configuration   = var.oke_workers_configuration
